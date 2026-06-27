1ac60b0f28c9852066178ad751c2f9810aac85f2
sfee1212 <102144622+sfee1212@users.noreply.github.com>
2026-06-27 11:34:58 +0800

    Update fhx.js

--- fhx.js
+++ fhx.js
@@ -1,6 +1,6 @@
 const PHONE_PREFIX = "86";
-const PHONE = "替换成您的手机号"; // 在这里填入您的凤凰秀手机号
-const PWD = "替换成您的密码";     // 在这里填入您的凤凰秀密码
+const PHONE = "18062763898"; // 在这里填入您的凤凰秀手机号
+const PWD = "li123123";     // 在这里填入您的凤凰秀密码
 
 function readToken() {
     try {
